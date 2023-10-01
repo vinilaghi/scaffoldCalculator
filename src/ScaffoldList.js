984fccfae840245d99a2ebd257f45badf320d859
vinilaghi <vinicius.laghi@gmail.com>
2023-10-01 17:48:56 -0300

    Fixed math and sframe

--- src/ScaffoldList.js
+++ src/ScaffoldList.js
@@ -27,67 +27,77 @@ const ScaffoldList = () => {
                     qseqid = scaffold.qseqid
                 }
                 if (!tempIdList[scaffold.scaffold_id]) {
-                    
                   tempIdList[scaffold.scaffold_id] = {}
-                  tempIdList[scaffold.scaffold_id][scaffold.qframe] = []
-                  tempIdList[scaffold.scaffold_id][scaffold.qframe].push({qstart: scaffold.qstart, qend: scaffold.qend})
+                  tempIdList[scaffold.scaffold_id][scaffold.sframe] = []
+                  tempIdList[scaffold.scaffold_id][scaffold.sframe].push({qstart: scaffold.qstart, qend: scaffold.qend})
                   } else {
-                    if (tempIdList[scaffold.scaffold_id][scaffold.qframe]) {
-                        tempIdList[scaffold.scaffold_id][scaffold.qframe].push({qstart: scaffold.qstart, qend: scaffold.qend})
+                    if (tempIdList[scaffold.scaffold_id][scaffold.sframe]) {
+                        tempIdList[scaffold.scaffold_id][scaffold.sframe].push({qstart: scaffold.qstart, qend: scaffold.qend})
                     } else {
-                        tempIdList[scaffold.scaffold_id][scaffold.qframe] = []
-                        tempIdList[scaffold.scaffold_id][scaffold.qframe].push({qstart: scaffold.qstart, qend: scaffold.qend})
+                        tempIdList[scaffold.scaffold_id][scaffold.sframe] = []
+                        tempIdList[scaffold.scaffold_id][scaffold.sframe].push({qstart: scaffold.qstart, qend: scaffold.qend})
                     }
                   }
             })
+
             Object.keys(tempIdList).forEach(key => {
                 let newScaffold = key;
-                Object.keys(tempIdList[key]).forEach(frame => {
-                    tempIdList[key][frame].forEach(item => {
-                        item.qstart = parseInt(item.qstart);
-                        item.qend = parseInt(item.qend);
-                    })
-                    let tempResults = 0
-                    if (parseInt(frame) < 0) {
-                        let tempFrames = cloneDeep(tempIdList[key][frame])
+  
+
+                    Object.keys(tempIdList[key]).forEach(frame => {
                         
-                        tempFrames.sort((a, b) => {
-                            return a.qstart - b.qstart;
-                        });
-                        tempFrames.forEach((item, index) => {
-
-                            if (index < tempFrames.length -1) {
-                                let tempNumber = index +1;
-                                if (item.qend >= tempFrames[tempNumber].qstart){
-                                debugger;
-
-                                    tempFrames[tempNumber].qstart = item.qend +1
-                                }
+                            tempIdList[key][frame].forEach(item => {
+                                item.qstart = parseInt(item.qstart);
+                                item.qend = parseInt(item.qend);
+                            })
+                            let tempResults = 0
+                            if (parseInt(frame) < 0) {
+                               
+                                let tempFrames = cloneDeep(tempIdList[key][frame])
+                                
+                                tempFrames.sort((a, b) => {
+                                    return a.qstart - b.qstart;
+                                });
+                                console.log(tempFrames)
+                                tempFrames.forEach((item, index) => {
+            
+                                    if (parseInt(frame) === -2 && newScaffold === 'scaffold14') {
+                                        // debugger;
+                                    }
+                                    if (index < tempFrames.length -1) {
+                                        let tempNumber = index +1;
+                                        if (item.qend >= tempFrames[tempNumber].qstart){
+                                            tempFrames[tempNumber].qstart = item.qend +1
+                                        }
+                                    }
+                                    debugger;
+                                    let tempValue = item.qend - item.qstart;
+                                    tempResults +=  tempValue
+                                    debugger;
+                                })
+                            } else {
+                                let tempFrames = cloneDeep(tempIdList[key][frame])
+                                tempFrames.sort((a, b) => {
+                                    return a.qend - b.qend;
+                                });
+        
+                                tempFrames.forEach((item, index) => {
+                                    if (index < tempFrames.length -1) {
+                                        let tempNumber = index +1;
+        
+                                        if (item.qstart >= tempFrames[tempNumber].qend){
+                                            tempFrames[tempNumber].qend = item.qstart +1
+                                        }
+                                    }
+                                    tempResults =+ (item.qstart - item.qend)
+                                })
                             }
-                            tempResults =+ (item.qstart - item.qend)
-                        })
-                    } else {
-                        let tempFrames = cloneDeep(tempIdList[key][frame])
-                        tempFrames.sort((a, b) => {
-                            return a.qend - b.qend;
-                        });
-
-                        tempFrames.forEach((item, index) => {
-                            if (index < tempFrames.length -1) {
-                                let tempNumber = index +1;
-
-                                if (item.qstart >= tempFrames[tempNumber].qend){
-                                debugger;
-
-                                    tempFrames[tempNumber].qend = item.qstart +1
-                                }
-                            }
-                            tempResults =+ (item.qend - item.qstart)
-                        })
-                    }
-                    tempList.push('Em ' + qseqid + ' o frame ' + frame + ' do ' + newScaffold + ' tem uma cobertura de ' + (tempResults / qlen)*100)
-
-                })
+                            tempList.push('Em ' + qseqid + ' o frame ' + frame + ' do ' + newScaffold + ' tem uma cobertura de ' + (tempResults / qlen)*100)
+        
+                        
+                       
+                    })
+                
 
               });
 
